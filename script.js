768736682f269300b7f11d3d70e4dafa7e95a0c7
Asel Alimbaeva <asel_19.86@mail.ru>
2021-09-10 16:43:26 +0300

    add js file

---
+++ script.js
@@ -0,0 +1,108 @@
+// Объявить две целочисленные переменные a и b и задать им произвольные начальные значения. Затем написать скрипт, который работает по следующему принципу:
+// если a и b положительные, вывести их разность;
+// если а и b отрицательные, вывести их произведение;
+// если а и b разных знаков, вывести их сумму; ноль можно считать положительным числом.
+
+
+var a = -4;
+var b = 4;
+var c = 0;
+
+a = parseInt(Math.random() * b + a);
+console.log('a=' + a);
+
+b = parseInt(Math.random() * b + a);
+console.log('b=' + b);
+function NumberVal(a, b) {
+    if (a >= 0 && b >= 0) {
+        c = a * b;
+        console.log('a=' + a, ', b=' + b, ' a-b=' + c);
+    } if (a < 0 && b < 0) {
+        c = a * b;
+        console.log('a=' + a, ', b=' + b, ' a*b=' + c);
+    } else {
+        c = a + b;
+        console.log('a=' + a, ', b=' + b, ' a+b=' + c);
+    }
+}
+console.log(NumberVal(a, b));
+
+// Присвоить переменной а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от a до 15. 
+// Можно сделать решение двумя способами. Второй способ - использовать рекурсию, можно без switch
+
+alert('№ - 4');
+function showNumbersUnilZerro(n) {
+    if (n === 15) {
+        return 15;
+    }
+    console.log(n);
+    showNumbersUnilZerro(n + 1);
+
+}
+var d = 3;
+showNumbersUnilZerro(n = parseInt(Math.random() * 15 + d));
+
+
+
+// 5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
+
+
+
+alert('№ - 5');
+
+function summa(a, b) {
+    return a + b;
+};
+
+function minus(a, b) {
+    return a - b;
+};
+
+function mult(a, b) {
+    return a * b;
+};
+
+function division(a, b) {
+    if (b != 0)
+        return a / b;
+    return 0
+};
+
+
+console.log(summa(2, 3));
+console.log(minus(2, 3));
+console.log(mult(2, 3));
+console.log(division(2, 3));
+
+// 6. Реализовать функцию с тремя параметрами:
+//  function mathOperation(arg1, arg2, operation), 
+//  где arg1, arg2 – значения аргументов, operation – строка с названием операции.
+//   В зависимости от переданного значения операции выполнить одну из арифметических 
+//   операций (использовать функции из пункта 5) и вернуть полученное значение (использовать switch).
+
+function mathOperation(arg1, arg2, oper) {
+    switch (oper) {
+        case '+':
+            a = summa(arg1, arg2);
+            return a
+            break;
+        case '-':
+            a = minus(arg1, arg2);
+            return a
+            break;
+        case '*':
+            a = division(arg1, arg2);
+            return a
+            break;
+        case '/':
+            a = mult(arg1, arg2);
+            return a
+            break;
+    };
+};
+console.log(mathOperation(2, 3, '+'));
+
+
+// 8. *С помощью рекурсии организовать функцию возведения числа в степень.
+//  Формат: function power(val, pow), где val – заданное число, pow – степень.
+
